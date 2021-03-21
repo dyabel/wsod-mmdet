ee82a0e21c4fbd5a3c025e2a8f1ca4290d89fda5
dyabel <abelazady@foxmail.com>
2021-03-21 12:39:58 +0800

    update

--- load_proposals.py
+++
@@ -1,5 +0,0 @@
-import mmcv
-proposals = mmcv.load('../data/VOCdevkit/proposals/rpn_r101_fpn_voc_train.pkl')
-# proposals = mmcv.load('test.pkl')
-# mmcv.dump(proposals,'test.pkl')
-print(proposals)
