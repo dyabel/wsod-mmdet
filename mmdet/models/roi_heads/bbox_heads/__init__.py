b9f602b595c17df8c7e295812baef1f97159872b
dyabel <abelazady@foxmail.com>
2021-03-24 10:36:38 +0800

    delete

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -2,16 +2,12 @@ from .bbox_head import BBoxHead
 from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
                                Shared4Conv1FCBBoxHead)
 from .dii_head import DIIHead
-# from .oicr_bbox_head_copy import (ConvFCOICRHead,Shared2FCOICRHead)
-from .oicr_bbox_head import (ConvFCOICRHead,Shared2FCOICRHead)
-from .oicr_bbox_head_branch1 import (ConvFCOICRHeadBranch1,Shared2FCOICRHeadBranch1)
-from .oicr_bbox_head_branch2 import (ConvFCOICRHeadBranch2,Shared2FCOICRHeadBranch2)
 from .double_bbox_head import DoubleConvFCBBoxHead
 from .sabl_head import SABLHead
 from .wsod_bbox_head import ConvFCWSODHead,Shared2FCWSODHead
 
 __all__ = [
-    'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead','ConvFCOICRHead','Shared2FCOICRHead',
-    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'SABLHead', 'DIIHead','ConvFCOICRHeadBranch1','Shared2FCOICRHeadBranch1',
-    'ConvFCOICRHeadBranch2','Shared2FCOICRHeadBranch2','ConvFCWSODHead','Shared2FCWSODHead'
+    'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
+    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'SABLHead', 'DIIHead',
+    'ConvFCWSODHead','Shared2FCWSODHead'
 ]
