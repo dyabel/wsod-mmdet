c764f853e12642c3e132277165fe303ed0079f1c
dythu <abelazady@foxmail.com>
2021-08-07 21:18:37 +0800

    update

--- tools/load/load_proposals.py
+++ tools/load/load_proposals.py
@@ -1,6 +1,6 @@
 import mmcv
-# proposals = mmcv.load('../../../data/VOCdevkit/proposals/voc_2007_trainval.pkl')
-proposals = mmcv.load('/home/dy20/voc_2007_test.pkl')
+proposals = mmcv.load('../../../data/VOCdevkit/proposals/rpn_r101_fpn_voc_train.pkl')
+#proposals = mmcv.load('/home/dy20/voc_2007_test.pkl')
 # proposals = mmcv.load('test.pkl')
 # mmcv.dump(proposals,'test.pkl')
-print((proposals))
+print(len(proposals[0]))
